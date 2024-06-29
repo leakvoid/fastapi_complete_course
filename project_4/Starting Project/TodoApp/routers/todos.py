becbf4689b4a4d431a2bbe384210779adebc712a
Dmitry <leakvoid@gmail.com>
2024-06-29 18:37:29 +0300

    fastapi update project 4

---
+++ project_4/Starting Project/TodoApp/routers/todos.py
@@ -0,0 +1,60 @@
+import sys
+sys.path.append("..")
+
+from typing import Optional
+from fastapi import Depends, HTTPException, APIRouter, Request
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from .auth import get_current_user, get_user_exception
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(
+    prefix="/todos",
+    tags=["todos"],
+    responses={404: {"description": "Not found"}}
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def read_all_by_user(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@router.get("/add-todo", response_class=HTMLResponse)
+async def add_new_todo(request: Request):
+    return templates.TemplateResponse("add-todo.html", {"request": request})
+
+@router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo(request: Request):
+    return templates.TemplateResponse("edit-todo.html", {"request": request})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
